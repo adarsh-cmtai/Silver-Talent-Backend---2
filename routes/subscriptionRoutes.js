2ea70cfe55fe83180aa7383936950c2103ac9d03
adarsh-cmtai <adarsh.shahi@completrix.com>
2025-07-10 12:19:47 +0530

    Update subscriptionRoutes.js

--- routes/subscriptionRoutes.js
+++ routes/subscriptionRoutes.js
@@ -1,8 +1,10 @@
-// backend/routes/subscriptionRoutes.js
 const express = require('express');
 const router = express.Router();
 const subscriptionController = require('../controllers/subscriptionController');
 
 router.post('/subscribe', subscriptionController.subscribeToAlerts);
+router.get('/subscriptions', subscriptionController.getAllSubscriptions);
+router.put('/subscriptions/:id', subscriptionController.updateSubscription);
+router.delete('/subscriptions/:id', subscriptionController.deleteSubscription);
 
 module.exports = router;
