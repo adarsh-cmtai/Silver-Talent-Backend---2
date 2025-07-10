58e75af8cee2af9b40a995b4a792b0276f0d4001
adarsh-cmtai <adarsh.shahi@completrix.com>
2025-07-10 12:20:13 +0530

    Update Subscription.js

--- models/Subscription.js
+++ models/Subscription.js
@@ -1,11 +1,10 @@
-// backend/models/Subscription.js
 const mongoose = require('mongoose');
 
 const subscriptionSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
-        unique: true, // Ensure no duplicate email subscriptions
+        unique: true,
         trim: true,
         lowercase: true,
         match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Please provide a valid email address'],
